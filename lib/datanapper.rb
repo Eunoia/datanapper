33876bec9731bb5f7a387b85e3049d0fe86cc96b
Evan R <eunoia.github@gmail.com>
2015-09-06 14:07:11 -0700

    does not work with odd segments

--- lib/datanapper.rb
+++ lib/datanapper.rb
@@ -2,6 +2,10 @@ require "datanapper/version"
 
 module DataNapper
   def self.math(string)
-  	return -1
+  	matches = string.scan(/(\d+)([\+-\/\*])(\d+)/).each_slice(3).inject(0) do |a,b,d|
+  		a += b.inject(0) do |a_,b_,d_|
+	  		a_+=Integer(b_[0]).send(b_[1], Integer(b_[2]))
+	  	end
+  	end
   end
 end
--- spec/datanapper_spec.rb
+++ spec/datanapper_spec.rb
@@ -1,37 +1,37 @@
 require 'spec_helper'
 
 describe DataNapper do
-  describe '#math(5+5)' do
+  xdescribe '#math(5+5)' do
     it 'does simple addition' do
       result = DataNapper.math('5+5')
       expect(result).to eq(10)
     end
   end
-  describe '#math(92432+123355)' do
+  xdescribe '#math(92432+123355)' do
     it 'does multi digit addition' do
       result = DataNapper.math('92432+123355')
       expect(result).to eq(215_787)
     end
   end
-  describe '#math(924+32+123+355)' do
+  xdescribe '#math(924+32+123+355)' do
     it 'does multi digit, multi part addition' do
       result = DataNapper.math('924+32+123+355')
       expect(result).to eq(1_434)
     end
   end  
-  describe '#math(5-5)' do
+  xdescribe '#math(5-5)' do
     it 'does simple subtraction' do
       result = DataNapper.math('5-5')
       expect(result).to eq(0)
     end
   end
   describe '#math(747-73-232)' do
-    it 'does multi digit, multi part subtraction' do
+    it 'does multi digit, odd-multi part subtraction' do
       result = DataNapper.math('747-73-232')
       expect(result).to eq(442)
     end
   end  
-  describe '#math(92432-13355)' do
+  xdescribe '#math(92432-13355)' do
     it 'does multi digit subtraction' do
       result = DataNapper.math('92432-13355')
       expect(result).to eq(79_077)
