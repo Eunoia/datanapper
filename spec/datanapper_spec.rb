b12aa009242cdada90e2cfbb1ec7519902c3f15b
Evan R <eunoia.github@gmail.com>
2015-09-06 14:19:36 -0700

    Added tests for division and multiplication

--- spec/datanapper_spec.rb
+++ spec/datanapper_spec.rb
@@ -36,5 +36,41 @@ describe DataNapper do
       result = DataNapper.math('92432-13355')
       expect(result).to eq(79_077)
     end
+  end
+  describe '#math(5*5)' do
+    it 'does simple multiplation' do
+      result = DataNapper.math('5*5')
+      expect(result).to eq(25)
+    end
+  end
+  describe '#math(747*73*232)' do
+    it 'does multi digit, odd-multi part multiplation' do
+      result = DataNapper.math('747*73*232')
+      expect(result).to eq(12_651_192)
+    end
+  end  
+  describe '#math(92432*13355)' do
+    it 'does multi digit multiplation' do
+      result = DataNapper.math('92432*13355')
+      expect(result).to eq(1_234_429_360)
+    end
+  end
+  describe '#math(5/5)' do
+    it 'does simple division' do
+      result = DataNapper.math('5/5')
+      expect(result).to eq(1)
+    end
+  end
+  describe '#math(747/83/3)' do
+    it 'does multi digit, odd-multi part 747/83/3' do
+      result = DataNapper.math('747/83/3')
+      expect(result).to eq(3)
+    end
+  end  
+  describe '#math(92432/53)' do
+    it 'does multi digit 92432/53' do
+      result = DataNapper.math('92432/53')
+      expect(result).to eq(1_744)
+    end
   end  
 end
