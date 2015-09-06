9403d57bb6f0a4b94b09031ad12d3fecfb3bc088
Evan R <eunoia.github@gmail.com>
2015-09-06 14:15:59 -0700

    works for multipart expressions

--- lib/datanapper.rb
+++ lib/datanapper.rb
@@ -1,11 +1,13 @@
 require "datanapper/version"
-
+$c =0
 module DataNapper
+	OPERATOR_PRECEDENCE = "*/+-"
   def self.math(string)
-  	matches = string.scan(/(\d+)([\+-\/\*])(\d+)/).each_slice(3).inject(0) do |a,b,d|
-  		a += b.inject(0) do |a_,b_,d_|
-	  		a_+=Integer(b_[0]).send(b_[1], Integer(b_[2]))
-	  	end
+  	while string.match(/[\+-\/\*]/) do
+  		matches = string.scan(/(\d+)([\+-\/\*])(\d+)/)
+  		match = matches.pop
+  		string.gsub!(match.join, Integer(match[0]).send(match[1], Integer(match[2])).to_s)
   	end
+  	string.to_i
   end
 end
--- spec/datanapper_spec.rb
+++ spec/datanapper_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
 describe DataNapper do
-  xdescribe '#math(5+5)' do
+  describe '#math(5+5)' do
     it 'does simple addition' do
       result = DataNapper.math('5+5')
       expect(result).to eq(10)
     end
   end
-  xdescribe '#math(92432+123355)' do
+  describe '#math(92432+123355)' do
     it 'does multi digit addition' do
       result = DataNapper.math('92432+123355')
       expect(result).to eq(215_787)
     end
   end
-  xdescribe '#math(924+32+123+355)' do
+  describe '#math(924+32+123+355)' do
     it 'does multi digit, multi part addition' do
       result = DataNapper.math('924+32+123+355')
       expect(result).to eq(1_434)
     end
   end  
-  xdescribe '#math(5-5)' do
+  describe '#math(5-5)' do
     it 'does simple subtraction' do
       result = DataNapper.math('5-5')
       expect(result).to eq(0)
@@ -31,7 +31,7 @@ describe DataNapper do
       expect(result).to eq(442)
     end
   end  
-  xdescribe '#math(92432-13355)' do
+  describe '#math(92432-13355)' do
     it 'does multi digit subtraction' do
       result = DataNapper.math('92432-13355')
       expect(result).to eq(79_077)
